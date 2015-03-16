3fd6841d847dc50326750a64296f26bf2b12e0f6
ionuta <ionut.albastroiu@rga.com>
2015-03-16 23:23:02 +0200

    sdsd

--- src/main/java/PageConstants.java
+++ src/main/java/PageConstants.java
@@ -1,7 +1,17 @@
 package main.java;
 
+import org.openqa.selenium.By;
+
 /**
  * Created by ionuta on 3/16/15.
  */
-public class PageConstants {
+public class PageConstants  {
+
+            public static final By GET_A_PERSONAL_ASSISTANT = By.linkText("Get a personal assistant");
+            public static final By PLAN_MY_JOURNEY =By.linkText("Plan my journey");
+
+
+
+
+
 }
