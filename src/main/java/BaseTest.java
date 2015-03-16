801c18c446a8ebaa06141e42d2bc17751cb6dd08
ionuta <ionut.albastroiu@rga.com>
2015-03-16 23:22:53 +0200

    sdsd

--- src/main/java/BaseTest.java
+++ src/main/java/BaseTest.java
@@ -1,7 +1,43 @@
 package main.java;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by ionuta on 3/16/15.
  */
-public class BaseTest {
+public abstract class BaseTest {
+
+    protected static WebDriver driver;
+
+
+    @BeforeClass
+    public static void openBrowser() {
+       if (driver == null){
+           driver = new FirefoxDriver();
+       }
+
+    }
+    @AfterClass
+    public static void closeBrowser() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    protected BasePage  loadBasePage (){
+        driver.get("https://trylumiaphone.com");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        return new BasePage(driver);
+
+
+
+    }
 }
