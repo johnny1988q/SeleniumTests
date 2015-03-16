c9d1d6e2fa606e6cd9255549092a1d5546da405e
ionuta <ionut.albastroiu@rga.com>
2015-03-16 23:23:10 +0200

    sdsd

--- src/main/java/SummaryPage.java
+++ src/main/java/SummaryPage.java
@@ -1,7 +1,24 @@
 package main.java;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 /**
  * Created by ionuta on 3/16/15.
  */
-public class SummaryPage {
+
+public class SummaryPage extends BasePage{
+
+    public static final By SUMMARY_BODY = By.className("summary");
+
+
+SummaryPage (WebDriver driver){
+super (driver);
+
+
+
+}
+
+
+
 }
